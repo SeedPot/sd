d2bdc2a33bc87ef8eaf5f4fdd6c34361ebf35d57
TheFauno <daza.camilos@gmail.com>
2016-12-22 22:39:41 -0300

    cliente udp

--- UDP/Cliente.java
+++ UDP/Cliente.java
@@ -2,96 +2,73 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class Cliente{
-	
-	public static void main(String args[]){
+public class Cliente {
 
+	public static void main(String args[]) {
+		String token;
+		Int calculo;
 		//Menu
 		Scanner sc = new Scanner(System.in);
 		String rut, pass, union;
 		int tipoCon;
+
 		System.out.println("\n\n\tServicios Calculo de Series\n");
 		System.out.println("\tInicio de sesion");
 		//
-		System.out.print("\n\tRUT: ");			
+		System.out.print("\n\tRUT: ");
 		rut = sc.nextLine();
 		//password
 		System.out.print("\tPASSWORD: ");
 		pass = sc.nextLine();
 		//solicitar tipo conexion 
 		System.out.println("\nTipo de conexion: ");
-		System.out.println("1:UDP, 2:TCP, 3:Serialización");	
+		System.out.println("1:UDP, 2:TCP, 3:Serialización");
 		tipoCon = sc.nextLine();
 
 		switch (tipoCon) {
+		case 1:
+			int serie;
+			int n;
+			ConexionUDP c = new ConexionUDP();
+			token = c.login(rut, clave);
+			System.out.println("Token: " + token + "\n");
+
+			System.out.println("Seleccione la serie que desea calcular: ");
+			System.out.println("1: Fibonacci 2:Taylor");
+
+			Scanner sc = new Scanner();
+			serie = Int.toInt(sc.nextLine());
+
+			System.out.println("Seleccione hasta donde desea calcular n");
+			n = Int.toInteger(sc.nextLine());
+
+			switch (serie) {
 			case 1:
-				ConexionUDP c = new ConexionUDP();
-				if(c.login(rut, clave)){
-
-					String serie;
-					System.out.println("Conexion realizada con éxito");
-					System.out.println("Seleccione la serie que dessea calcular: ");
-
-					Scanner sc  = new Scanner();
-					 serie = sc.nextLine();
-
-				}
+				//Fibonacci
+				calculo = c.serie(rut, token, serie, n);
 				break;
 			case 2:
-				//TCP
+				//Taylor	
+				calculo = c.serie(rut, token, serie, n);
 				break;
-
-			case 3:
-				//serializacion
-				break;
-
 			default:
-				System.out.println("1: UDP , 2:TCP , 3:Serialización");
+				System.out.println("Selecciona una opcion válida");
 				break;
-		}
-		
 
-		try{
-			DatagramSocket socket = new DatagramSocket();
-			InetAddress host = InetAddress.getLocalHost();
-			int puerto = 6789;
+			}
+			System.out.println("El resultado es: " + calculo);
+			break;
+		case 2:
+			//TCP
+			break;
 
-			
+		case 3:
+			//serializacion
+			break;
 
-			union = rut + ":" + pass;
-			byte[] credenciales = union.getBytes("UTF-8");
-
-			DatagramPacket peticion = new DatagramPacket(	credenciales, 
-															union.length(), 
-															host, 
-															puerto
-															);
-			socket.send(peticion);
-
-
-
-
-
-														//mensaje, largo, host, puerto
-			//DatagramPacket peticion = new DatagramPacket(m, args[0].length(), host, puerto);
-			//socket.send(peticion);
-
-
-
-			byte[] bufer = new byte[1000];
-			
-			DatagramPacket respuesta = new DatagramPacket(bufer, bufer.length);
-			socket.receive(respuesta);
-			System.out.println("\nDefinicion: \n" + new String(respuesta.getData()).trim() + "\n");
-			
-			socket.close();
-		}
-		catch(SocketException e){
-			System.out.println("Socket: " + e.getMessage());
-		}
-		catch(IOException e){
-			System.out.println("IO: " + e.getMessage());
+		default:
+			System.out.println("1: UDP , 2:TCP , 3:Serialización");
+			break;
 		}
 	}
-	
 }
--- UDP/ConexionUDP.java
+++ UDP/ConexionUDP.java
@@ -15,11 +15,41 @@ public class ConexionUDP{
         this.host = InetAddress.getLocalHost();
     }
 
-    public bool login(String rut, String clave){
+    public String login(String rut, String clave){
+        String union;
+        String respuesta;
         Login login = new Login();
         String clavEn = login.encriptar(clave);
-        union = rut + ":" + pass;
-        return true;
+
+        union = "1:" + rut + ":" + claveEn;
+
+        return respuesta = this.request(union);
+    }
+
+    public int serie(String rut, String token,String idSerie, int n){
+        String union;
+        String respuesta;
+        //series 1: fibonacci 2:taylor    
+
+        union = "2:" + "rut:" + "token:" + n;
+         
+        return respuesta = this.request(union);
+    }
+
+    public String  request(String mensaje){
+        
+        byte[] mensajeBytes = mensaje.getBytes("UTF-8");
+		DatagramPacket peticion = new DatagramPacket(mensajeBytes, mensaje.length(), this.host, this.puerto);
+		socket.send(peticion);
+
+        byte[] bufer = new byte[1000];
+			
+		DatagramPacket respuesta = new DatagramPacket(bufer, bufer.length);
+		socket.receive(respuesta);
+		socket.close();
+
+        return new String(respuesta.getData()).trim();
+
     }
 
 }
